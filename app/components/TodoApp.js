986811fcf036025a42732465491b545cc8b34e4a
Lorenzo Beltrame <lorenzo.beltrame@gmail.com>
2018-02-07 14:59:40 +0700

    added todo list

--- app/components/TodoApp.js
+++ app/components/TodoApp.js
@@ -1,10 +1,33 @@
 var React = require('react');
-
+var TodoList = require('TodoList');
 var TodoApp = React.createClass({
-    render: function() {
-        return(
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Leave mail on porch'
+                },
+                {
+                    id: 4,
+                    text: 'Brush your teeth'
+                }
+            ]
+        };
+    },
+    render: function () {
+        var { todos } = this.state;
+        return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos} />
             </div>
         )
     }
